7b4e24b8549578bc63e849bf5fb40531ba1587cc
Ihor Orlovskyi <ihor.droid@gmail.com>
2015-11-26 02:18:34 +0200

    add jquery.language.detection.min.js

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/**
+* languageDetection
+* https://github.com/danieledesantis/jquery-language-detection
+*/
+$(function() {
+    $(document).languageDetection({
+        domain: '',
+        languages: [
+            {
+                code : 'en',
+                path : '/',
+                defaultLanguage : true
+            },
+            {
+                code : 'ru',
+                path : '/ru/'
+            }
+        ]
+    });
+});
